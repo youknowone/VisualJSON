a73a19905c91b43585ba34dab5481005b7e5a326
Jeong YunWon <jeong@youknowone.org>
2011-12-12 23:25:55 +0900

    add icon

--- VisualJSON/AppDelegate.m
+++ VisualJSON/AppDelegate.m
@@ -17,4 +17,13 @@
     // Insert code here to initialize your application
 }
 
+- (BOOL)applicationShouldOpenUntitledFile:(NSApplication *)sender {
+    return YES;
+}
+
+- (BOOL)applicationOpenUntitledFile:(NSApplication *)sender {
+    [self.window orderFrontRegardless];
+    return YES;
+}
+
 @end
